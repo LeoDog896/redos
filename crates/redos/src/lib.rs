355ce2cab722eadb50a63d833b841eebf3a3002e
Tristan F <LeoDog896@hotmail.com>
2023-09-18 12:24:57 -0400

    feat: check for complex regexes

--- crates/redos/src/lib.rs
+++ crates/redos/src/lib.rs
@@ -1,14 +1,45 @@
-use regex_full_syntax::{parse, Expression};
+use regex_full_syntax::{parse, Expression, Regex};
+
+/// Checks if the regex is "complex" (or has any quantifiers)
+pub fn is_simple_regex(regex: Regex) -> bool {
+    for alternation in regex.0 {
+        for expression in alternation {
+            match expression {
+                Expression::Group(group) => {
+                    if group.quantifier.is_none() {
+                        continue;
+                    }
+
+                    return is_simple_regex(group.regex);
+                },
+                Expression::String(str) => {
+                    for char in str {
+                        if char.quantifier.is_some() {
+                            return false;
+                        }
+                    }
+                },
+                Expression::CharacterClass(_) => (),
+            }
+        }
+    }
+
+    true
+}
 
 /// Returns true if not vulnerable, false otherwise
 pub fn safe(regex: &str) -> bool {
     let regex = parse(regex).unwrap();
     for alternation in regex.0 {
         for expression in alternation {
+            // looking for groups - if there's no groups, we can assume its safe
             if let Expression::Group(group) = expression {
-                if group.quantifier.is_some() {
-                    return false;
+                // and the group must have a quantifier
+                if group.quantifier.is_none() {
+                    continue;
                 }
+
+                return is_simple_regex(group.regex);
             }
         }
     }
