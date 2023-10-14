0afaa1ce45f071e56a3bba4e41bfa9675305bcf0
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-10-14 10:09:51 -0400

    perf: use Cow for return tpye

--- crates/redos/src/parse.rs
+++ crates/redos/src/parse.rs
@@ -3,105 +3,121 @@
 //! Links:
 //!     - <https://docs.python.org/3/library/re.html#regular-expression-syntax>
 
+use std::borrow::Cow;
+
 use crate::vulnerability::Vulnerability;
 use nom::{
     branch::alt,
     bytes::complete::{tag, take},
     character::complete::{digit1, one_of},
-    combinator::{map, opt, recognize},
-    multi::{many0, separated_list0},
+    combinator::{map, opt, recognize, value},
+    multi::{many0, many_m_n, separated_list0},
     sequence::{delimited, pair, preceded, separated_pair},
     IResult, Parser,
 };
 
+fn cow<'a, B, I, O, E, F>(mut parser: F) -> impl FnMut(I) -> IResult<I, Cow<'a, B>, E>
+where
+    F: Parser<I, O, E>,
+    B: ToOwned + ?Sized + 'a,
+    O: Clone + Into<Cow<'a, B>>,
+{
+    move |input: I| {
+        let (input, o1) = parser.parse(input)?;
+        Ok((input, o1.into()))
+    }
+}
+
 /// Parses regex character literals, returning a string that can match with it
-fn literal(i: &str) -> IResult<&str, &str> {
-    alt((
+fn literal(i: &str) -> IResult<&str, Cow<str>> {
+    cow(alt((
         // escape characters
-        tag("\\n").map(|_| "\n"),
-        tag("\\t").map(|_| "\t"),
-        tag("\\r").map(|_| "\r"),
-        tag("\\v").map(|_| "\x0B"),
-        tag("\\f").map(|_| "\x0C"),
-        tag("\\0").map(|_| "\0"),
-        tag("\\x").map(|_| "\0"),
-        // TODO: unicode support for \uXXXX and \x{XXXX}
+        value("\n", tag("\\n")),
+        value("\t", tag("\\t")),
+        value("\r", tag("\\r")),
+        value("\x0B", tag("\\v")),
+        value("\x0C", tag("\\f")),
+        value("\0", tag("\\0")),
+        value("\0", tag("\\x")),
+        // preceded(tag("\\u"), many_m_n(4, 4, recognize(one_of("0123456789abcdef"))))
+        //     .map(|hex| std::char::from_u32(u32::from_str_radix(hex.join(""), 16).unwrap()).unwrap().to_string()),
+        // TODO: unicode support for \x{XXXX}
         // TODO: unicode categories
         // general escape character
         preceded(tag("\\"), take(1_usize)),
-        tag("\\\\").map(|_| "\\"),
+        value("\\\\", tag("\\")),
         // character classes
-        tag("\\d").map(|_| "0"),
-        tag("\\D").map(|_| "D"),
-        tag("\\w").map(|_| "w"),
-        tag("\\W").map(|_| "0"),
-        tag("\\s").map(|_| " "),
-        tag("\\S").map(|_| "S"),
+        value("0", tag("\\d")),
+        value("D", tag("\\D")),
+        value("w", tag("\\w")),
+        value("0", tag("\\W")),
+        value(" ", tag("\\s")),
+        value("S", tag("\\S")),
         // other symbols: we can just put "." as a dot since it matches everything
         // (refactor)TODO: better alphanumeric support (alphanumberic1 exists but it matches multiple)
         recognize(one_of(
             "!@#%&_~`.<>/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
         )),
-    ))(i)
+    )))(i)
 }
 
 /// Parse regex character literals outside of a character class
-fn regex_literal(i: &str) -> IResult<&str, &str> {
-    alt((literal, recognize(one_of("-"))))(i)
+fn regex_literal(i: &str) -> IResult<&str, Cow<str>> {
+    alt((literal, cow(recognize(one_of("-")))))(i)
 }
 
 /// Parses regex character literals, laxed since they're inside a character class
-fn character_class_literal(i: &str) -> IResult<&str, &str> {
+fn character_class_literal(i: &str) -> IResult<&str, Cow<str>> {
     alt((
         literal,
-        recognize(one_of("$^|.?*{}[()")),
+        cow(recognize(one_of("$^|.?*{}[()"))),
         // we dont need to check for \\] since we do that in literal already
     ))(i)
 }
 
 /// Parses a range to be used inside a character class
-fn range(i: &str) -> IResult<&str, (&str, &str)> {
+fn range(i: &str) -> IResult<&str, (Cow<str>, Cow<str>)> {
     separated_pair(character_class_literal, tag("-"), character_class_literal)(i)
 }
 
 /// Parses a character class, returning a string that can match with it.
 /// We must own the returned String here, as this function sometimes doesn't
 /// produce a result owned by the input string.
-fn character_class(i: &str) -> IResult<&str, Option<String>> {
+fn character_class(i: &str) -> IResult<&str, Option<Cow<str>>> {
     delimited(
         tag("["),
         alt((
             preceded(
                 tag("^"),
                 // we turn the literal into a subrange
-                many0(alt((range, character_class_literal.map(|x| (x, x))))),
+                many0(alt((
+                    range,
+                    character_class_literal.map(|x| (x.clone(), x)),
+                ))),
             )
-            .map(|negation| {
+            .map(|negation| -> Option<Cow<str>> {
                 if negation.is_empty() {
-                    Some(".".to_string()) // [^] is the same as . in regex
+                    Some(".".into()) // [^] is the same as . in regex
                 } else {
                     // turn the negation vec into a hashmap
                     let mut negation_map = std::collections::HashMap::new();
                     for (start, end) in negation {
-                        negation_map.insert(start, end);
+                        negation_map.insert(start.to_string(), end.to_string());
                     }
 
                     // go through every unicode character and check if it's in the negation map, till we find one that isn't
                     let mut i = 0;
                     while i < std::u32::MAX {
                         let c = std::char::from_u32(i).unwrap();
-                        if negation_map.contains_key(&c.to_string().as_str()) {
+                        if negation_map.contains_key(&c.to_string()) {
                             // if we're at the end of the map, we can just return None
                             if i == std::u32::MAX - 1 {
                                 return None;
                             }
 
-                            i = negation_map[&c.to_string().as_str()]
-                                .chars()
-                                .next()
-                                .unwrap() as u32;
+                            i = negation_map[&c.to_string()].chars().next().unwrap() as u32;
                         } else {
-                            return Some(c.to_string());
+                            return Some(Cow::Owned(c.to_string()));
                         }
 
                         i += 1;
@@ -112,8 +128,11 @@ fn character_class(i: &str) -> IResult<&str, Option<String>> {
             }),
             // we can just get the first char here - ranges don't truly matter
             // TODO: [] sets don't match with *anything*
-            many0(alt((character_class_literal, tag("-"))))
-                .map(|s| s.first().map(|x| x.to_string())),
+            many0(alt((
+                character_class_literal,
+                cow::<str, _, _, _, _>(tag("-")),
+            )))
+            .map(|s| s.first().cloned()),
         )),
         tag("]"),
     )(i)
@@ -148,9 +167,9 @@ fn quantifier(i: &str) -> IResult<&str, Quantifier> {
     map(
         pair(
             alt((
-                map(tag("*"), |_| (0, None)),
-                map(tag("+"), |_| (1, None)),
-                map(tag("?"), |_| (0, Some(1))),
+                value((0, None), tag("*")),
+                value((1, None), tag("+")),
+                value((0, Some(1)), tag("?")),
                 map(
                     delimited(
                         tag("{"),
@@ -182,22 +201,22 @@ fn quantifier(i: &str) -> IResult<&str, Quantifier> {
 }
 
 /// Parses a group, returning an attack string & potential vulnerabilities.
-fn group(i: &str) -> IResult<&str, Vec<Vec<Option<String>>>> {
+fn group(i: &str) -> IResult<&str, Vec<Vec<Option<Cow<str>>>>> {
     // TODO: support group types
     delimited(tag("("), regex, tag(")"))(i)
 }
 
 /// Parses a "piece" of a regex, i.e. a single group or char, and returns an attack string
-fn piece(i: &str) -> IResult<&str, Option<String>> {
+fn piece(i: &str) -> IResult<&str, Option<Cow<str>>> {
     // TODO: group support
     // TODO: actual detection
     // TODO: quantifier support (lazy & non-lazy)
-    alt((character_class, regex_literal.map(|x| Some(x.to_string()))))(i)
+    alt((character_class, regex_literal.map(Some)))(i)
 }
 
 /// Parses every alternation of a regex, returning a Vec of Vec<attack strings>.
 /// Each element in the top-level Vec is a different alternation.
-fn regex(i: &str) -> IResult<&str, Vec<Vec<Option<String>>>> {
+fn regex(i: &str) -> IResult<&str, Vec<Vec<Option<Cow<str>>>>> {
     // TODO: proper parsing support for cancelled alternations (e.g. a||b)
     separated_list0(tag("|"), many0(piece))(i)
 }
@@ -208,9 +227,9 @@ mod tests {
 
     #[test]
     fn character_class_hit() {
-        assert_eq!(character_class("[ba-cd]"), Ok(("", Some("b".to_string()))));
-        assert_eq!(character_class("[-token]"), Ok(("", Some("-".to_string()))));
-        assert_eq!(character_class("[^a]"), Ok(("", Some("\x00".to_string()))));
+        assert_eq!(character_class("[ba-cd]"), Ok(("", Some("b".into()))));
+        assert_eq!(character_class("[-token]"), Ok(("", Some("-".into()))));
+        assert_eq!(character_class("[^a]"), Ok(("", Some("\x00".into()))));
         // TODO: when we have unicode ranges, test without a null char.
     }
 
