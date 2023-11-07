f746c17b1f24bc07179e0405cfeeef119603df50
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-11-06 21:30:32 -0500

    chore: init wasm package

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import init from 'redos';
+	import { onMount } from 'svelte';
+    onMount(() => {
+        init().then(() => {
+            // greet();
+        });
+    })
+</script>
