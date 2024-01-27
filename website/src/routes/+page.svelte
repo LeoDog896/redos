e35857f6e45501f9b79b6f96e99a7c98edf9548a
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-01-27 17:19:29 -0500

    feat: base webpage example

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
-    import init from 'redos';
+    // @ts-ignore
+    import init, { parse } from 'redos-wasm';
 	import { onMount } from 'svelte';
+
+    let hasBeenEnabled = false;
+
     onMount(() => {
-        init().then(() => {
-            // greet();
-        });
+        init().then(() => hasBeenEnabled = true);
     })
+
+    let regex = "";
+    let value = "";
+
+    $: if (hasBeenEnabled) {
+        value = parse(regex)
+    }
 </script>
+
+<input bind:value={regex} />
+
+<pre>
+    {value}
+</pre>
--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,7 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import wasmPack from 'vite-plugin-wasm-pack';
+import topLevelAwait from "vite-plugin-top-level-await";
+import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
-	plugins: [sveltekit(), wasmPack("../crates/redos")]
+	plugins: [sveltekit(), wasm(), topLevelAwait(), wasmPack(["../crates/redos-wasm"])]
 });
